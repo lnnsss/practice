269ba220fb2823af5bc4e5ad653625153657e805
lns <142323634+lnnsss@users.noreply.github.com>
2024-11-27 09:46:12 +0300

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -8,77 +8,77 @@ import dotenv from 'dotenv';
 dotenv.config();
 const secret = process.env.JWT_SECRET;
 
-// Регистрация
-export const registration = async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array());
+export default class AuthController {
+    static async registration(req, res) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array());
+            }
+    
+            const password = req.body.password;
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(password, salt);
+    
+            const userDoc = new UserModel({
+                email: req.body.email,
+                passwordHash: hash,
+            });
+    
+            const user = await userDoc.save();
+    
+            const newCart = await CartModel.create({ userId: user._id });
+    
+            user.cartId = newCart._id;
+            await user.save();
+    
+            const token = jwt.sign({
+                _id: user._id,
+            }, secret, {
+                expiresIn: '24h',
+            });
+    
+            const { passwordHash, ...userData } = user._doc;
+    
+            res.json({ ...userData, token });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: 'Не удалось зарегистрироваться',
+            });
         }
-
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-
-        const userDoc = new UserModel({
-            email: req.body.email,
-            passwordHash: hash,
-        });
-
-        const user = await userDoc.save();
-
-        const newCart = await CartModel.create({ userId: user._id });
-
-        user.cartId = newCart._id;
-        await user.save();
-
-        const token = jwt.sign({
-            _id: user._id,
-        }, secret, {
-            expiresIn: '24h',
-        });
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({ ...userData, token });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось зарегистрироваться',
-        });
     }
-};
-
-// Вход в аккаунт
-export const login = async (req, res) => {
-    try {
-        const user = await UserModel.findOne({email: req.body.email});
-        if (!user) {
-            return res.status(404).json({
-                message: 'Польщователь не найден',
+    static async login(req, res) {
+        try {
+            const user = await UserModel.findOne({email: req.body.email});
+            if (!user) {
+                return res.status(404).json({
+                    message: 'Польщователь не найден',
+                })
+            }
+    
+            const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+            if (!isValidPass) {
+                return res.status(400).json({
+                    message: 'Неверный логин или пароль',
+                })
+            }
+    
+            const token = jwt.sign({
+                _id: user._id,
+            }, secret, {
+                expiresIn: '24h',
+            })
+    
+            const {passwordHash, ...userData} = user._doc;
+    
+            res.json({...userData, token});
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: 'Не удалось авторизоваться',
             })
         }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'Неверный логин или пароль',
-            })
-        }
-
-        const token = jwt.sign({
-            _id: user._id,
-        }, secret, {
-            expiresIn: '24h',
-        })
-
-        const {passwordHash, ...userData} = user._doc;
-
-        res.json({...userData, token});
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось авторизоваться',
-        })
     }
-};
+    
+}
