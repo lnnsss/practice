92b4a30fec110e600e81c82f83c4563643314923
lns <142323634+lnnsss@users.noreply.github.com>
2024-11-29 09:45:53 +0300

    Update auth.js

--- validations/auth.js
+++ validations/auth.js
@@ -1,6 +1,10 @@
 import { body } from 'express-validator';
 
 export const registerValidation = [
-    body('email').isEmail(),
-    body('password').isLength({min: 6})
+    body('email').isEmail().withMessage('Неверный формат электронной почты'),
+    body('password')
+        .isLength({ min: 6 }).withMessage('Пароль должен содержать не меньше 6 символов')
+        .matches(/[a-z]/).withMessage('Пароль должен содержать строчные буквы')
+        .matches(/[A-Z]/).withMessage('Пароль должен содержать заглавные буквы')
+        .matches(/[!#$%&?]/).withMessage('Пароль должен содержать специальные символы (! # $ % & ?)')
 ];
